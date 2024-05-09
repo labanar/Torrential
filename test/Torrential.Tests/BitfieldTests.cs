ad0384a9ab846a7e6a84008e0730e9707d3b2649
Rami Labana <labanar@live.com>
2024-05-08 20:03:42 -0400

    update bitfield tests

--- test/Torrential.Tests/BitfieldTests.cs
+++ test/Torrential.Tests/BitfieldTests.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Torrential.Peers;
+﻿using Torrential.Peers;
 
 namespace Torrential.Tests
 {
-    
+
     public class BitfieldTests
     {
         [Fact]
@@ -44,5 +39,72 @@ namespace Torrential.Tests
             //We should no longer be suggesting pieces if we have the all
             Assert.Null(myBitfield.SuggestPieceToDownload(peerBitfield).Index);
         }
+
+
+        [Fact]
+        public async Task Has_all_with_partial_final_byte_async()
+        {
+            var myBitfield = new AsyncBitfield(2516);
+            var peerBitfieldData = new byte[315];
+            Array.Fill(peerBitfieldData, (byte)255);
+
+            var peerBitfield = new AsyncBitfield(peerBitfieldData);
+
+            //Stage my bitfield to have all but the final byte set
+            var myPeerBitfieldData = new byte[314];
+            Array.Fill(myPeerBitfieldData, (byte)255);
+            myBitfield.Fill(myPeerBitfieldData);
+
+            Assert.False(myBitfield.HasAll());
+            var suggestedPiece = myBitfield.SuggestPieceToDownload(peerBitfield);
+            Assert.NotNull(suggestedPiece.Index);
+
+            await myBitfield.MarkHaveAsync(2512, CancellationToken.None);
+            Assert.False(myBitfield.HasAll());
+
+            await myBitfield.MarkHaveAsync(2513, CancellationToken.None);
+            Assert.False(myBitfield.HasAll());
+
+            await myBitfield.MarkHaveAsync(2514, CancellationToken.None);
+            Assert.False(myBitfield.HasAll());
+
+            await myBitfield.MarkHaveAsync(2515, CancellationToken.None);
+            Assert.True(myBitfield.HasAll());
+            Assert.Equal(1.0, myBitfield.CompletionRatio, 0);
+
+            //We should no longer be suggesting pieces if we have the all
+            Assert.Null(myBitfield.SuggestPieceToDownload(peerBitfield).Index);
+        }
+
+
+        [Fact]
+        public void Has_all_with_partial_final_byte_alt()
+        {
+            var myBitfield = new Bitfield(2106);
+            var peerBitfieldData = new byte[264];
+            Array.Fill(peerBitfieldData, (byte)255);
+
+            var peerBitfield = new Bitfield(peerBitfieldData);
+
+            //Stage my bitfield to have all but the final byte set
+            var myPeerBitfieldData = new byte[263];
+            Array.Fill(myPeerBitfieldData, (byte)255);
+            myBitfield.Fill(myPeerBitfieldData);
+
+            Assert.False(myBitfield.HasAll());
+            var suggestedPiece = myBitfield.SuggestPieceToDownload(peerBitfield);
+            Assert.NotNull(suggestedPiece.Index);
+
+            myBitfield.MarkHave(2104);
+            Assert.False(myBitfield.HasAll());
+
+            myBitfield.MarkHave(2105);
+            Assert.True(myBitfield.HasAll());
+            Assert.Equal(1.0, myBitfield.CompletionRatio, 0);
+
+            //We should no longer be suggesting pieces if we have the all
+            Assert.Null(myBitfield.SuggestPieceToDownload(peerBitfield).Index);
+        }
+
     }
 }
