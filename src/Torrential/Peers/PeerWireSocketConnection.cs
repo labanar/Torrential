1cae844603c9ce7a2eb265d8ef0a747969c8898c
Rami Labana <labanar@live.com>
2024-05-12 08:23:45 -0400

    leverage LINQ .Sum to benefit from SIMD
    - cleanup listener

--- src/Torrential/Peers/PeerWireSocketConnection.cs
+++ src/Torrential/Peers/PeerWireSocketConnection.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
 using System.IO.Pipelines;
-using System.Net;
 using System.Net.Sockets;
-using Torrential.Torrents;
 using Torrential.Trackers;
+using Torrential.Utilities;
 
 namespace Torrential.Peers;
 
@@ -14,22 +13,18 @@ public class PeerWireSocketConnection : IPeerWireConnection
     private readonly Task _ingressFillTask;
     private readonly Pipe _egressPipe;
     private readonly Task _egressFillTask;
-    private readonly NetworkStream _stream;
     private readonly HandshakeService _handshakeService;
     private readonly ILogger _logger;
-    private readonly TorrentMetadataCache _metaCache;
 
     public Guid Id { get; }
 
-    public PeerInfo PeerInfo { get; private set; }
 
+
+    public PeerInfo? PeerInfo { get; private set; }
     public PeerId? PeerId { get; private set; }
-
     public InfoHash InfoHash { get; private set; } = InfoHash.None;
     public bool IsConnected { get; private set; }
-
     public DateTimeOffset ConnectionTimestamp { get; private set; } = DateTimeOffset.UtcNow;
-
     public PipeReader Reader { get; }
     public PipeWriter Writer { get; }
 
@@ -61,11 +56,7 @@ public class PeerWireSocketConnection : IPeerWireConnection
             return PeerConnectionResult.Failure;
         }
 
-        PeerInfo = new PeerInfo
-        {
-            Ip = ((IPEndPoint)_socket.RemoteEndPoint).Address,
-            Port = ((IPEndPoint)_socket.RemoteEndPoint).Port
-        };
+        PeerInfo = _socket.GetPeerInfo();
         PeerId = peerHandshake.PeerId;
         InfoHash = peerHandshake.InfoHash;
         IsConnected = true;
@@ -136,15 +127,24 @@ public class PeerWireSocketConnection : IPeerWireConnection
     {
         while (true)
         {
+
             ReadResult result = await reader.ReadAsync();
             var buffer = result.Buffer;
 
-            foreach (var segment in buffer)
+            try
             {
-                await socket.SendAsync(segment, SocketFlags.None);
-            }
+                foreach (var segment in buffer)
+                {
+                    await socket.SendAsync(segment, SocketFlags.None);
+                }
 
-            reader.AdvanceTo(buffer.End);
+                reader.AdvanceTo(buffer.End);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error writing to peer");
+                break;
+            }
 
             if (result.IsCompleted)
             {
@@ -154,46 +154,6 @@ public class PeerWireSocketConnection : IPeerWireConnection
 
         reader.Complete();
     }
-
-    ////Read from the pipe and 
-    //async Task IngressReadPipeAsync(PipeReader reader)
-    //{
-    //    while (true)
-    //    {
-    //        ReadResult result = await reader.ReadAsync();
-
-    //        ReadOnlySequence<byte> buffer = result.Buffer;
-    //        SequencePosition? position = null;
-
-    //        do
-    //        {
-    //            // Look for a EOL in the buffer
-    //            position = buffer.PositionOf((byte)'\n');
-
-    //            if (position != null)
-    //            {
-    //                // Process the line
-    //                ProcessLine(buffer.Slice(0, position.Value));
-
-    //                // Skip the line + the \n character (basically position)
-    //                buffer = buffer.Slice(buffer.GetPosition(1, position.Value));
-    //            }
-    //        }
-    //        while (position != null);
-
-    //        // Tell the PipeReader how much of the buffer we have consumed
-    //        reader.AdvanceTo(buffer.Start, buffer.End);
-
-    //        // Stop reading if there's no more data coming
-    //        if (result.IsCompleted)
-    //        {
-    //            break;
-    //        }
-    //    }
-
-    //    // Mark the PipeReader as complete
-    //    reader.Complete();
-    //}
 
 
     public void Dispose()
