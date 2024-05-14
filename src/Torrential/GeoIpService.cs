676bf3208d9cc6935cc854b7cd03901c03498229
Rami Labana <labanar@live.com>
2024-05-14 18:01:56 -0400

    add geofencing

---
+++ src/Torrential/GeoIpService.cs
@@ -0,0 +1,19 @@
+﻿using MaxMind.Db;
+using MaxMind.GeoIP2;
+
+namespace Torrential
+{
+    public sealed class GeoIpService
+    {
+        private readonly DatabaseReader _countryDbReader = new(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "GeoLite2-Country.mmdb"), FileAccessMode.Memory);
+
+        public ValueTask<string> GetCountryCodeAsync(string ip)
+        {
+            var country = _countryDbReader.Country(ip);
+            if (country.Country == null)
+                return new ValueTask<string>("");
+
+            return new ValueTask<string>(country.Country.IsoCode ?? "");
+        }
+    }
+}
